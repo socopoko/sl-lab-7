2cf36abdd18e8c3d141b7b9054e2636a57ee2241
socopoko <59167401+socopoko@users.noreply.github.com>
2020-09-28 11:43:39 +0530

    Made all the required components

---
+++ src/app/todo/todo-footer/todo-footer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-todo-footer',
+  templateUrl: './todo-footer.component.html',
+  styleUrls: ['./todo-footer.component.css']
+})
+export class TodoFooterComponent implements OnInit {
+
+  @Input() count: any;
+ 
+  constructor() { 
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+}
